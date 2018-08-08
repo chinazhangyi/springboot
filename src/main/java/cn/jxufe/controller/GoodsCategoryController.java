8beae8b12821a85282b6bad49b784dc0344a05ad
chinazhangyi <15270762624@163.com>
2018-08-08 16:57:04 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	pom.xml
    #	springboot.iml
    #	src/main/java/cn/jxufe/controller/PageController.java

---
+++ src/main/java/cn/jxufe/controller/GoodsCategoryController.java
@@ -0,0 +1,31 @@
+package cn.jxufe.controller;
+
+import cn.jxufe.entity.GoodsCategory;
+import cn.jxufe.entity.Goodsinfo;
+import cn.jxufe.service.GoodsCategoryService;
+import cn.jxufe.service.GoodsInfoServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * Created by Zhang on 2018/8/8.
+ */
+@Controller
+public class GoodsCategoryController {
+    @Autowired
+    private GoodsCategoryService goodsCategoryService;
+    @Autowired
+    private GoodsInfoServer goodsInfoServer;
+    @RequestMapping("category")
+    public String findtype(ModelMap map,Integer catId){
+        List<GoodsCategory> gtype = goodsCategoryService.findtype();
+        List<Goodsinfo> goodsByType = goodsInfoServer.findGoodsByType(catId);
+        map.put("goodsByType",goodsByType);
+        map.put("gtype",gtype);
+        return "category";
+    }
+}
